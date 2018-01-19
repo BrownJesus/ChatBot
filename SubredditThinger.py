d1adb317f2287b7decba229562db24aefefcaf92
Joseph Acevedo <jacevedo@bitbuckets.org>
2018-01-18 18:09:02 -0700

    Added minimum score checking and comment cleaning
    Formats the comment to remove escape characters and checks for minimum score

--- SubredditThinger.py
+++ SubredditThinger.py
@@ -1,5 +1,9 @@
+import string
+
 readFile  = "RC_2009-03"
 writeFile = "RC_2009-03_Republican.txt"
+
+MINIMUM_SCORE = 5
 
 def main():
     fileOpen  = open(readFile, 'r')
@@ -8,15 +12,46 @@ def main():
     lines    = fileOpen.readlines()
     comments = {}
 
+    dataFound = 0
+
     for line in lines:
         if line.find("\"subreddit\":\"politics\"") != -1: # In politics subreddit
-            # fileWrite.write(findScore(line))
-            fileWrite.write(findBody(line))
+            score = findScore(line)
+            if int(score) > MINIMUM_SCORE:
+                dataFound += 1
+                body = formatLine(findBody(line))
+                fileWrite.write(body)
         else:
             continue
 
     fileOpen.close()
     fileWrite.close()
+    print(str(dataFound) + " valid comments with a score of more than " + str(MINIMUM_SCORE) + " found!")
+
+def formatLine(line):
+    while line.find("\\t") != -1:
+        pos = line.find("\\t")
+        line = line.replace(line[pos:pos+2], "")
+    while line.find("\\n") != -1:
+        pos = line.find("\\n")
+        line = line.replace(line[pos:pos+2], "")
+    while line.find("\\\\") != -1:
+        pos = line.find("\\\\")
+        line = line.replace(line[pos:pos+2], "")
+    while line.find("\\\"") != -1:
+        pos = line.find("\\\"")
+        line = line.replace(line[pos:pos+2], "")
+    while line.find("\\u") != -1:
+        pos = line.find("\\u")
+        line = line.replace(line[pos:pos+6], "")
+    while line.find("\\r") != -1:
+        pos = line.find("\\r")
+        line = line.replace(line[pos:pos+2], "")
+    while line.find("[deleted]") != -1:
+        pos = line.find("[deleted]")
+        line = line.replace(line[pos:pos+9], "")
+    return line
+        
 
 # Takes in a line and returns the value to write to the file
 def findScore(line):
